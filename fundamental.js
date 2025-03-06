03d16575f8c609a23bd07dc1cb14af29ba8140f2
Tranquilogabe <calummamarket@gmail.com>
2025-03-06 00:00:51 -0300

    first

---
+++ fundamental.js
@@ -0,0 +1,78 @@
+//maneira antiga de armazenar valores em variavel
+var myVariable = "Hello World";
+ console.log(myVariable);
+
+
+ //Variaveis e tipos de dados:
+
+ var myNumber = 10;
+ console.log( myNumber);
+
+ console.log(myNumber + 5);
+
+
+ //metedodo para identificar tipo de dado TYPEOF
+ console.log(typeof myNumber);
+
+ var myBoolean = true;
+ var myArray = [];
+ var myObject = {};
+ var myNull = null;
+ var myUndefined = undefined;
+
+
+ console.log(typeof myBoolean);
+ console.log(typeof myArray);
+ console.log(typeof myObject);
+ console.log(typeof myNull);
+ console.log(typeof myUndefined);
+
+
+// maneira atual de definir variavel
+//let & const
+// de maneira geral devemos utilizar mais o const, para evitar erros 
+
+
+let x = 30;
+const y = 90;
+
+
+//operador de comparacao
+console.log(x==y);
+
+console.log("5" == 5);
+console.log("5" === 5);
+
+// novamente de maneira geral utilizaremos "===" devido a menor incidencia de erros 
+//aqui acima teremos resultados diferentes, pois um operador traz a igualdade restrita e o ooutro apenas a nominal.
+
+
+
+//OPREADORES LOGICOS
+//AND "&&"
+//OR  "||"
+
+
+console.log(5>6 && 7<6);
+console.log(5>6 || 7<6);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
